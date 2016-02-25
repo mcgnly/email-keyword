999004057c46801614f71efe7e2e8a9dadbc44f9
Katie McGinley <katie@mcgnly.com>
2016-02-25 12:05:45 +0100

    remove db duplication

--- client/emailKeywordClient.js
+++ client/emailKeywordClient.js
@@ -1,19 +1,3 @@
-// what's the deal with the caps and non-caps?
-KeywordCollection = new Mongo.Collection('keywordCollection');
-
-//disallow the client side from doing anything directly
-KeywordCollection.allow({
-    insert: function(userId, doc){
-        return false;
-    },
-    update: function(userId, doc, fields, modifier) {
-        return false;
-    },
-    remove: function (userId, doc){
-        return false;
-    }
-});
-
 if (Meteor.isClient) {
 
 	console.log('Clients rock, servers suck')
