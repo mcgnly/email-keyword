a9be6ab8d6ff6bb8da3e84b0e43b9020578be142
Katie McGinley <katie@mcgnly.com>
2016-05-14 16:35:13 +0200

    email working

--- server/envVars.js
+++ server/envVars.js
@@ -1,3 +1,4 @@
 Meteor.startup(function() {
-    process.env.MAIL_URL = 'smtp://postmaster@mcgnly.com:232425m@smtp.mailgun.org:587';
+    process.env.MAIL_URL = 'smtp://postmaster@mcgnly.com:ed3e4cd8a4915f35eb28e9918518a38c@smtp.mailgun.org:587';
+
 });
