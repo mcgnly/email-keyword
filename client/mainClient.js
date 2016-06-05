293438c84391f3ccff773d18a8868e0617f741cf
Katie McGinley <katie@mcgnly.com>
2016-06-05 14:30:49 +0200

    this is a bit of a mess

--- client/mainClient.js
+++ client/mainClient.js
@@ -2,6 +2,8 @@ if (Meteor.isClient) {
 
     console.log('Clients rock, servers suck')
 
+
+
     Template.body.helpers({
         // hardcode the object:
         // keywordobject: [
@@ -16,58 +18,64 @@ if (Meteor.isClient) {
     });
 
     Template.body.events({
-        'submit .new-email': function(event) { //event holds the info fron the forms
-            var thisemail = event.target.email.value;
-            var thiskeyword = event.target.keyword.value;
-            //call the server mthd bc client isn't allowed to DO shit
-            Meteor.call('addKeyword', thisemail, thiskeyword)
-            //clear out the form box and false to stop an unnecessary refresh
-            event.target.email.value = "";
-            event.target.keyword.value = "";
-            // toastr.success("thanks!", "your email was successfully added to our database!");
+            'submit .new-email': function(event) { //event holds the info fron the forms
+                var thisemail = event.target.email.value;
+                var thiskeyword = event.target.keyword.value;
+                //call the server mthd bc client isn't allowed to DO shit
+                Meteor.call('addToMailingList', user.email, (error) => {
+                        if (error) {
+                            console.log(error.reason);
+                        }
 
-            return false;
-        },
 
-        'click .twitterChecker': function(event) {
-            Meteor.call('twitterChecker', function(error, result) {
-                if (error) {
-                    console.log('SERVER ERRR');
-                    console.log(error);
-                } else {
-                    console.log("checked twitter");
-                }
 
+
+                // Meteor.call('addKeyword', thisemail, thiskeyword)
+                        //clear out the form box and false to stop an unnecessary refresh
+                        event.target.email.value = "";
+                        event.target.keyword.value = "";
+                        // toastr.success("thanks!", "your email was successfully added to our database!");
+
+                        return false;
+                    },
+
+                    'click .twitterChecker': function(event) {
+                        Meteor.call('twitterChecker', function(error, result) {
+                            if (error) {
+                                console.log('SERVER ERRR');
+                                console.log(error);
+                            } else {
+                                console.log("checked twitter");
+                            }
+
+                        });
+                    }
+
+                });
+
+            Template.keywordTemplate.helpers({
+                // 'tweetText': function () {
+                // 	Meteor.call('tweetText')
+                // } 
             });
+
+            Template.keywordTemplate.events({ //this is holding an object of a bunch of "k/val pairs", hence {} and ,s
+                'click .toggle-checked': function() { //when the toggle-checked class in html is clicked (aka the key), do the fn (the value in the pair)
+                    Meteor.call('changeChecked', !this.checked, this._id) //send over the opposite of the checkedness, and the _id
+                }, //comma bc it's items in an object!
+                'click .delete': function() {
+                    Meteor.call('allowDelete', this._id)
+                }
+            });
+
         }
 
-    });
-
-    Template.keywordTemplate.helpers({
-        // 'tweetText': function () {
-        // 	Meteor.call('tweetText')
-        // } 
-    });
-
-    Template.keywordTemplate.events({ //this is holding an object of a bunch of "k/val pairs", hence {} and ,s
-        'click .toggle-checked': function() { //when the toggle-checked class in html is clicked (aka the key), do the fn (the value in the pair)
-            Meteor.call('changeChecked', !this.checked, this._id) //send over the opposite of the checkedness, and the _id
-        }, //comma bc it's items in an object!
-        'click .delete': function() {
-            Meteor.call('allowDelete', this._id)
-        }
-    });
-
-}
-
-Router.route('/about', {
-    name: 'about',
-    template: 'about'
-});
-Router.route('/', {
-    name: 'home',
-    template: 'home'
-});
-Router.configure({
-    layoutTemplate: 'main'
-});
+        Router.route('/about', {
+            name: 'about',
+            template: 'about'
+        }); Router.route('/', {
+            name: 'home',
+            template: 'home'
+        }); Router.configure({
+            layoutTemplate: 'main'
+        });
