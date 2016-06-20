c8c94eb998fda870f767ce0603f6e96164c41da6
Katie McGinley <katie@mcgnly.com>
2016-06-20 12:48:06 +0200

    email working now

--- server/mainServer.js
+++ server/mainServer.js
@@ -1,104 +1,110 @@
-    if (Meteor.isServer) {
+if (Meteor.isServer) {
 
-        console.log('Clients suck, servers rock')
+    console.log('Clients suck, servers rock')
 
-        var Twit = Meteor.npmRequire("twit");
-        // let twitSettings = Meteor.settings.private.twitter,
-        T = new Twit({
-            consumer_key: Meteor.settings.private.twitter.CONSUMER_KEY,
-            consumer_secret: Meteor.settings.private.twitter.CONSUMER_SECRET,
-            access_token: Meteor.settings.private.twitter.ACCESS_TOKEN,
-            access_token_secret: Meteor.settings.private.twitter.ACCESS_TOKEN_SECRET
-        });
+    var Twit = Meteor.npmRequire("twit");
+    // let twitSettings = Meteor.settings.private.twitter,
+    T = new Twit({
+        consumer_key: Meteor.settings.private.twitter.CONSUMER_KEY,
+        consumer_secret: Meteor.settings.private.twitter.CONSUMER_SECRET,
+        access_token: Meteor.settings.private.twitter.ACCESS_TOKEN,
+        access_token_secret: Meteor.settings.private.twitter.ACCESS_TOKEN_SECRET
+    });
 
-        let mgSettings = Meteor.settings.private.mailgun;
-        mg = new Mailgun({
-            apiKey: Meteor.settings.private.mailgun.MAILGUN_KEY,
-            domain: Meteor.settings.private.mailgun.domain
-        })
+    let mgSettings = Meteor.settings.private.mailgun;
+    mg = new Mailgun({
+        apiKey: Meteor.settings.private.mailgun.MAILGUN_KEY,
+        domain: Meteor.settings.private.mailgun.domain
+    })
 
-        SyncedCron.start();
+    SyncedCron.start();
 
-        Meteor.methods({
+    Meteor.methods({
 
-            'allowDelete': function(id) {
-                KeywordCollection.remove(id);
-            },
+        'allowDelete': function(id) {
+            KeywordCollection.remove(id);
+        },
 
-            'sendEmail': function(email) {
-                Email.send({
-                    to: email,
-                    sender: 'postmaster@mcgnly.com',
-                    from: 'postmaster@.mcgnly.com',
-                    subject: 'Testing testing',
-                    text: 'did it work?'
-                });
-                console.log("email sent")
-            },
+        'sendEmail': function(email) {
+            // check([to, from, subject, text], [String]);
+            Email.send({
+                to: email,
+                mail_from: 'postmaster@.mcgnly.com',
+                // sender: 'postmaster@mcgnly.com',
+                from: Meteor.settings.private.mailgun.from,
+                subject: 'Testing testing',
+                text: 'did it work?'
+            });
+            console.log("email sent")
+        },
 
-            'twitterChecker': function() {
-                // Construct the API URL and query the API
-                lastTweets = T.get('statuses/user_timeline', {
-                    screen_name: 'amandapalmer',
-                    // since_id: 735184012562436096,
-                    since_id: (LastTweetsCollection.findOne({}, {
-                        sort: {
-                            createdAt: -1
-                        }
-                    }) || {}).tweetId,
-                    trim_user: true,
-                    include_entities: false,
-                    include_rts: false
-                })
-
-                .catch(function(err) {
-                    console.log('caught error', err.stack)
-                })
-                    .then(Meteor.bindEnvironment(function(result) {
-                        // `result` is an Object with keys "data" and "resp".
-                        // `data` and `resp` are the same objects as the ones passed
-                        // to the callback.
-                        // See https://github.com/ttezel/twit#tgetpath-params-callback
-                        // for details.
-                        if (result.data.length > 0) {
-                            for (d in result.data) {
-
-                                var tweet = result.data[d].text;
-
-                                console.log(tweet);
-                                Meteor.call('tweetParser', tweet)
-                            }
-
-                            LastTweetsCollection.insert({
-                                tweetId: result.data[0].id_str,
-                                createdAt: new Date()
-                            })
-                        } else {
-                            console.log("nothing new to report");
-                        }
-                    }))
-            },
-
-            'tweetParser': function(tweet) {
-                var splitTweet = tweet.split(" ");
-                // console.log(splitTweet);
-
-                for (i = 0; i < splitTweet.length; i++) {
-                    twitterTrigger = KeywordCollection.findOne({
-                        keyword: splitTweet[i]
-                    });
-                    //this returns an object which will be true if it exists
-                    //if it DOES exist already...
-                    if (twitterTrigger) {
-                        console.log("got to twittertrigger");
-                        _.each(twitterTrigger.emails, function(email) {
-                            Meteor.call('sendEmail', email);
-                        });
+        'twitterChecker': function() {
+            // Construct the API URL and query the API
+            lastTweets = T.get('statuses/user_timeline', {
+                screen_name: 'amandapalmer',
+                // since_id: 735184012562436096,
+                since_id: (LastTweetsCollection.findOne({}, {
+                    sort: {
+                        createdAt: -1
                     }
-                }
-            },
+                }) || {}).tweetId,
+                trim_user: true,
+                include_entities: false,
+                include_rts: false
+            })
 
-            'addKeyword': function(email, keyword) {
+            .catch(function(err) {
+                console.log('caught error', err.stack)
+            })
+                .then(Meteor.bindEnvironment(function(result) {
+                    // `result` is an Object with keys "data" and "resp".
+                    // `data` and `resp` are the same objects as the ones passed
+                    // to the callback.
+                    // See https://github.com/ttezel/twit#tgetpath-params-callback
+                    // for details.
+                    if (result.data.length > 0) {
+                        for (d in result.data) {
+
+                            var tweet = result.data[d].text;
+
+                            console.log(tweet);
+                            Meteor.call('tweetParser', tweet)
+                        }
+
+                        LastTweetsCollection.insert({
+                            tweetId: result.data[0].id_str,
+                            createdAt: new Date()
+                        })
+                    } else {
+                        console.log("nothing new to report");
+                    }
+                }))
+        },
+
+        'tweetParser': function(tweet) {
+            var splitTweet = tweet.split(" ");
+            // console.log(splitTweet);
+
+            for (i = 0; i < splitTweet.length; i++) {
+                twitterTrigger = KeywordCollection.findOne({
+                    keyword: splitTweet[i]
+                });
+                //this returns an object which will be true if it exists
+                //if it DOES exist already...
+                if (twitterTrigger) {
+                    console.log("twittertrigger", twitterTrigger);
+                    _.each(twitterTrigger.emails, function(email) {
+                        Meteor.call('sendEmail', email);
+                    });
+                }
+            }
+        },
+
+        'addKeyword': function(email, keyword) {
+            if (!(email || keyword)) {
+                throw new Error('Provide an email and a keyword');
+            } else {
+
                 //returns true if the keyword given in the form matches something in the collection
                 existingKeyword = KeywordCollection.findOne({
                     keyword: keyword
@@ -128,8 +134,28 @@
                     });
                 }
             }
-        });
+        },
 
-        Meteor.startup(function() {});
+        'checkUnsubscribes': function() {
+            HTTP.call("GET", "https://api.mailgun.net/v3/mcgnly.com/unsubscribes", {
+                    auth: 'api:' + Meteor.settings.private.mailgun.MAILGUN_KEY
+                },
+                function(error, result) {
+                    if (!error) {
+                        console.log(result.content);
+                        // Meteor.call('deleteUnsubscribes', email);
+                    } else {
+                        console.log(error);
+                    }
+                });
+        },
 
-    }
+        'deleteUnsubscribes': function() {
+
+        }
+    });
+
+    Meteor.startup(function() {
+        process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
+    });
+}
